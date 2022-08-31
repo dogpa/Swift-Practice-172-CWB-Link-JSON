20c2ea008afce1c9f3dc645ac0a6106f6da84a16
Dogpa <dogpa0130@gmail.com>
2022-08-31 08:40:52 +0800

    Initial Commit

---
+++ Swift Practice 172 CWB Link JSON/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Swift Practice 172 CWB Link JSON
+//
+//  Created by Dogpa's MBAir M1 on 2022/8/31.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Swift Practice 172 CWB Link JSON/Swift_Practice_172_CWB_Link_JSONApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_Practice_172_CWB_Link_JSONApp.swift
+//  Swift Practice 172 CWB Link JSON
+//
+//  Created by Dogpa's MBAir M1 on 2022/8/31.
+//
+
+import SwiftUI
+
+@main
+struct Swift_Practice_172_CWB_Link_JSONApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
